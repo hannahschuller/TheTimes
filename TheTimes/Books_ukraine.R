7a74b266a7a3d465630ccb70608baa699eabfe33
DPKW9 <hannahschuller1@gmail.com>
2025-02-07 10:58:48 +0000

    Pushing books

--- TheTimes/Books_ukraine.R
+++ TheTimes/Books_ukraine.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyr) 
 library(lubridate)
+library(sf)
 
 book_revenue <- read.csv("/Users/hannah/Desktop/TheTimes/TheTimes/book_revenue.csv")
 
@@ -13,7 +14,7 @@ book_revenue <- book_revenue %>%
                values_to = "Revenue Change") %>%
   mutate(Year = as.integer(sub("X", "", Year))) 
 
-# Plotting the data
+# Plot 1: Bar chart
 ggplot(book_revenue, aes(x = factor(Year), y = `Revenue Change`, fill = X)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(title = "Book revenue change in Ukraine", 
@@ -31,3 +32,70 @@ ggplot(book_revenue, aes(x = factor(Year), y = `Revenue Change`, fill = X)) +
                                "Physical books" = "#f6c55e", 
                                "Total" = "#b01424"))
 
+
+bookshops <- read.csv("/Users/hannah/Desktop/TheTimes/TheTimes/Bookshops.csv")
+ukraine <- st_read("/Users/hannah/Desktop/TheTimes/TheTimes/gadm41_UKR_0.shp")
+
+# Converting dataset into a spatial object
+bookshops <- bookshops %>%
+  separate(point, into = c("latitude", "longitude"), sep = ", ", convert = TRUE)
+bookshops_sf <- st_as_sf(bookshops, coords = c("longitude", "latitude"), crs = 4326)
+
+# Make translated version
+bookshops_translated <- bookshops_sf %>%
+  mutate(name = recode(name,
+                       "Дім книги" = "House of Books",
+                       "Мережа книгарень Vivat" = "Vivat",
+                       "Мережа книгарень VALLIZA" = "Valliza",
+                       "Клуб Сімейного Дозвілля" = "KSD Book Club",
+                       "Книгарні-кав'ярні старого лева" = "Old Lion",
+                       "Мережа книгарень Книголенд" = "Knigoland Books",
+                       "Мережа Книгарень «Є»" = "The Ye Bookstore",
+                       "Мережа книгарень Буква" = "Bukva",
+                       "Немережеві книгарні" = "Other"
+  ))
+
+# Recoding for datawrapper
+bookshops_recoded <- bookshops %>%
+  mutate(name = recode(name,
+                       "Дім книги" = "House of Books",
+                       "Мережа книгарень Vivat" = "Vivat",
+                       "Мережа книгарень VALLIZA" = "Valliza",
+                       "Клуб Сімейного Дозвілля" = "KSD Book Club",
+                       "Книгарні-кав'ярні старого лева" = "Old Lion",
+                       "Мережа книгарень Книголенд" = "Knigoland Books",
+                       "Мережа Книгарень «Є»" = "The Ye Bookstore",
+                       "Мережа книгарень Буква" = "Bukva",
+                       "Немережеві книгарні" = "Other"
+  ))
+
+write.csv(bookshops_recoded, "bookshops_recoded.csv", row.names = FALSE)
+
+# Plot 2: Map of bookshops
+ggplot() +
+  geom_sf(data = ukraine, fill = NA, color = "black") +
+  geom_sf(data = bookshops_translated, aes(color = name), size = 1) +
+  scale_color_manual(values = c(
+    "House of Books" = "#ae1722",
+    "Vivat" = "#7fb1e2",
+    "Valliza" = "#f37f2f",
+    "KSD Book Club" = "#e0ab26",
+    "Old Lion" = "#f6c55e",
+    "Knigoland Books" = "#feea82",
+    "The Ye Bookstore" = "#254252",
+    "Bukva" = "#ed5156",
+    "Other" = "#578ab9"
+  )) +
+  labs(title = "Bookshops across Ukraine", color = NULL) +
+  theme_minimal() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    plot.title = element_text(face = "bold", hjust = 0, family = "Arial", color = "black"),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.text = element_text(family = "Arial", color = "black"),
+    legend.title = element_text(family = "Arial", color = "black"),
+    legend.position = "top"
+  )
